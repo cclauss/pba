057811ff005dbaff5612452eaef29620f785c40e
Daniel Ho <daniel.ho@berkeley.edu>
2019-01-29 20:38:29 -0800

    adding grid serach code

---
+++ grid_search.py
@@ -0,0 +1,50 @@
+"""Train and evaluate models using augmentation schedules."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+
+import tensorflow as tf
+
+import ray
+from ray.tune import Trainable
+from ray.tune import grid_search, run_experiments
+
+from train_cifar import CifarModelTrainer
+from train import RayModel
+
+def main(_):
+    from setup import create_parser, create_hparams
+    FLAGS = create_parser("train")
+    hparams = create_hparams("train", FLAGS)
+
+    train_spec = {
+        "run": RayModel,
+        "resources_per_trial": {
+            "cpu": FLAGS.cpu,
+            "gpu": FLAGS.gpu
+        },
+        "stop": {
+            "training_iteration": hparams.num_epochs,
+        },
+        "config": hparams.values(),
+        "local_dir": FLAGS.local_dir,
+        "checkpoint_freq": FLAGS.checkpoint_freq,
+        "num_samples": 5
+    }
+
+    if FLAGS.restore:
+        train_spec["restore"] = FLAGS.restore
+
+    train_spec["config"]["lr"] = grid_search([0.0125, 0.025, 0.05, 0.1, 0.2])
+    train_spec["config"]["wd"] = grid_search([0.0005, 0.00125, 0.0025, 0.005, 0.01, 0.02])
+
+    ray.init()
+    run_experiments({FLAGS.name: train_spec}, verbose=False)
+
+
+if __name__ == '__main__':
+    tf.logging.set_verbosity(tf.logging.INFO)
+    tf.app.run()
---
+++ scripts/grid_search.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+export PYTHONPATH="$(pwd)"
+export OMP_NUM_THREADS=2
+
+# CUDA_VISIBLE_DEVICES=0 ./scripts/search.sh aug_11-23 |& tee search_aug_11-23-pt1.txt
+
+wrn_40_2_grid_search() {
+    echo "[bash] Grid Search w/ wrn_40_2, policy ${1}"
+    python grid_search.py \
+    --local_dir /data/dho/ray_results_2/grid_search \
+    --model_name wrn_40_2 \
+    --data_path /data/dho/datasets/cifar-10-batches-py --dataset cifar10 \
+    --train_size 4000 --val_size 0 --eval_test \
+    --checkpoint_freq 0 \
+    --gpu 0.24 --cpu 1 \
+    --use_hp_policy --hp_policy "/data/dho/pba/schedules/reduced_cifar_10/16_wrn.txt" \
+    --explore cifar10 \
+    --hp_policy_epochs 200 \
+    --aug_policy "$1" --name "aug_policy-$1"
+}
+
+if [ "$1" = "aug_11-23" ]; then
+    echo "[bash] $@"
+    wrn_40_2_grid_search 11-23
+else
+    echo "invalid args"
+fi
