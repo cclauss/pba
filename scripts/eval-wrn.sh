b2581a871f33582048fb54f60d621d4f4997e64a
Daniel Ho <daniel.ho@berkeley.edu>
2019-02-01 20:27:49 -0800

    simply eval args

--- scripts/eval-wrn.sh
+++ scripts/eval-wrn.sh
@@ -9,8 +9,8 @@ wrn_40_2_eval() {
     --data_path /data/dho/datasets/cifar-10-batches-py --dataset cifar10 \
     --train_size 4000 --val_size 0 --eval_test \
     --checkpoint_freq 0 \
-    --gpu 0.142 --cpu 2 \
-    --use_hp_policy --hp_policy "/data/dho/pba/$3" \
+    --gpu 0.19 --cpu 2 \
+    --use_hp_policy --hp_policy "/data/dho/pba/schedules/$3" \
     --explore cifar10 \
     --hp_policy_epochs 200 \
     --aug_policy "$1" --name "$3" --num_samples "$2"
@@ -25,14 +25,16 @@ wrn_28_10_eval() {
     --train_size 50000 --val_size 0 --eval_test \
     --checkpoint_freq 0 \
     --gpu 1 --cpu 4 \
-    --use_hp_policy --hp_policy "/data/dho/pba/$3" \
+    --use_hp_policy --hp_policy "/data/dho/pba/schedules/$3.txt" \
     --explore cifar10 \
     --hp_policy_epochs 200 \
     --aug_policy "$1" --name "$3" --num_samples "$2"
 }
 
 #  number of trials wrn-40-2, number of trials wrn-28-10 schedule
-# CUDA_VISIBLE_DEVICES=0 source ./eval-wrn.sh 5 1 /data/dho/pba/schedules/reduced_cifar_10/16_wrn.txt
+# CUDA_VISIBLE_DEVICES=0 ./scripts/eval-wrn.sh 5 1 reduced_cifar_10/16_wrn.txt
+# CUDA_VISIBLE_DEVICES=2,3,4 ./scripts/eval-wrn.sh 5 3 11-23_bair_policy_6 |& tee eval_logs/11-23_bair_policy_6.txt
+# CUDA_VISIBLE_DEVICES=5,6,7 ./scripts/eval-wrn.sh 5 3 11-23_c66_policy_9 |& tee eval_logs/11-23_c66_policy_9.txt
 
 wrn_28_10_eval 11-23 $2 $3
 wrn_40_2_eval 11-23 $1 $3
