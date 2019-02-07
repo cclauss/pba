5c8a79f56396a8bdeee3f4982f901c0369358cb2
Daniel Ho <daniel.ho@berkeley.edu>
2019-02-06 19:45:00 -0800

    add cf100

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -8,9 +8,18 @@ eval() {
     if [ "$3" = "r-cf10" ]; then
         size=4000
         name="reduced_cifar10_$1"
+        dataset="cifar10"
+        data_path="/data/dho/datasets/cifar-10-batches-py"
     elif [ "$3" = "cf10" ]; then
         size=50000
         name="cifar10_$1"
+        dataset="cifar10"
+        data_path="/data/dho/datasets/cifar-10-batches-py"
+    elif [ "$3" == "cf100" ]; then
+        size=50000
+        name="cifar100_$1"
+        dataset="cifar100"
+        data_path="/data/dho/datasets/cifar-100-python"
     else
         echo "invalid dataset"
     fi
@@ -18,7 +27,7 @@ eval() {
     python train.py \
     --local_dir /data/dho/ray_results_2/eval \
     --model_name "$1" \
-    --data_path /data/dho/datasets/cifar-10-batches-py --dataset cifar10 \
+    --data_path "$data_path" --dataset "$dataset" \
     --train_size "$size" --val_size 0 --eval_test \
     --checkpoint_freq 0 \
     --gpu 1 --cpu 3 \
@@ -28,6 +37,6 @@ eval() {
     --aug_policy cifar10 --name "$name" --num_samples "$2" > "eval_logs/$name.txt" 2>&1 &
 }
 
-# CUDA_VISIBLE_DEVICES=0 source ./scripts/eval-wrn.sh 5 1  reduced-cifar10
+# CUDA_VISIBLE_DEVICES=0 source ./scripts/eval-wrn.sh wrn 1 r-cf10
 # wrn shake-shake-32 shake-shake-96 shake-shake-112 pyramidnet
 eval $1 $2 $3
