ee257deefc42e93b4952213e02a34beac02986fb
Daniel Ho <daniel.ho@berkeley.edu>
2019-03-09 19:30:32 -0800

    adding rebuttal aa

--- scripts/rebuttal/svhn-rebuttal-aa.sh
+++ scripts/rebuttal/svhn-rebuttal-aa.sh
@@ -3,11 +3,11 @@
 eval_svhn() {
     python train.py \
     --local_dir /data/dho/ray_results_2/svhn \
-    --model_name shake_shake_96 --dataset "svhn-full" \
+    --model_name "$1" --dataset "svhn-full" \
     --train_size 604388 --val_size 0 --eval_test \
     --checkpoint_freq 5 \
     --gpu 1 --cpu 4 \
-    --epochs 160 --name "svhn_full_aa_$1" --num_samples 1
+    --epochs 160 --name "svhn_full_aa_$1" --num_samples "$2"
 }
 
 eval_svhn "$@"
