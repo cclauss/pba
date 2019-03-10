930202f376016537c0f9c1b701f0de3754fa16f3
Daniel Ho <daniel.ho@berkeley.edu>
2019-03-09 17:41:58 -0800

    adding rebuttal

---
+++ scripts/rebuttal/svhn-rebuttal.sh
@@ -0,0 +1,16 @@
+eval_svhn() {
+    echo "model: $1, trials: 4"
+
+    python train.py \
+    --local_dir /data/dho/ray_results_2/svhn \
+    --model_name "$1" --dataset "svhn-full" \
+    --train_size 604388 --val_size 0 --eval_test \
+    --checkpoint_freq 5 \
+    --gpu 1 --cpu 4 \
+    --use_hp_policy --hp_policy "/data/dho/pba/schedules/svhn/svhn_2_23_b_policy_15.txt" \
+    --explore cifar10 \
+    --hp_policy_epochs 160 --epochs 160 \
+    --aug_policy cifar10 --name "svhn_full_$1" --num_samples 4
+}
+
+eval_svhn "$@"
